459bffb2feea7d9dd824c467649b721e56386dbb
Nikita Singhal <nikitasinghal@Nikitas-MacBook-Air.local>
2025-08-30 17:45:10 +0530

    removing unnecessary comments

--- src/main/java/com/nikita/Codesage/service/OpenAIService.java
+++ src/main/java/com/nikita/Codesage/service/OpenAIService.java
@@ -35,8 +35,7 @@ public class OpenAIService {
             "content", "You are a senior code reviewer."
         );
 
-        // Dummy change to test CodeSage PR review
-        //change
+    
 
         Map<String, Object> messageUser = Map.of(
             "role", "user",
@@ -49,8 +48,6 @@ public class OpenAIService {
         );
 
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(requestBody, headers);
-
-        //another dummy change to test CodeSage PR review
         try {
             ResponseEntity<Map> response = restTemplate.postForEntity(url, request, Map.class);
             Map<String, Object> body = response.getBody();
